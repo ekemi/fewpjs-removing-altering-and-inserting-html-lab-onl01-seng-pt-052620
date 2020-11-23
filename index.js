0c0edc0ab5a0cf23650a7adc973e9ae48773b21a
edmundo papi <edmundopierre@yahoo.com>
2020-11-23 15:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 let r =document.getElementById("main").remove();
 let newHeader = document.createElement('h1');
-newHeader.setAttribute("id", "Victory");
+newHeader.setAttribute("id", "victory");
 newHeader.innerHTML = "Lolo is the champion";
